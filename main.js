ccff809797d0d1b670c41af202ca01b39c1bbafe
Diyora31 <asilovad7>
2024-04-12 16:32:50 +0500

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+let text1 = prompt('your name')
+let text2 = prompt('your lastname')
+let text3 = +prompt('your age')
+let text4 = prompt('your adress')
+let text5 = prompt('your favorite subject')
+
+
+console.log(`foydalanuvchi ismi:${text1} \nfoydalanuvchi familyasi:${text2}\nfoydalanuvchi yoshi:${text3}\nfoydalanuvchi manzili:${text4}\nsevimli fani:${text5}`);
+
+
+
+
